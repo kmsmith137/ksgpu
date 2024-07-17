8329f9b9966c54682744b2f9aaf729cd7ed83d96
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2024-07-17 17:45:23 -0400

    cupy juggling

--- src_python/ksgpu/__init__.py
+++ src_python/ksgpu/__init__.py
@@ -1,11 +1,9 @@
 import numpy
-import cupy
 
 # FIXME this makes dir(ksgpu) look weird, since it consists entirely of ad hoc functions
 # for testing. I'll probably clean this up when there's more python functionality in ksgpu.
 
 from .ksgpu_pybind11 import *
-
 
 def launch_busy_wait_kernel(arr, a40_seconds):
     """
@@ -16,5 +14,25 @@ def launch_busy_wait_kernel(arr, a40_seconds):
     The 'a40_seconds' arg determines the amount of work done by the kernel,
     normalized to "seconds on an NVIDIA A40".
     """
+
+    # We import cupy here, since putting 'import cupy' at the top of the file would lead
+    # to the following tragic sequence of events:
+    #
+    #   - "Downstream" modules (e.g. gpu_mm) must declare cupy as a build-time
+    #     dependency, since they 'import ksgpu' in order to get the location
+    #     of the ksgpu .hpp files.
+    #
+    #   - When a downstream module is installed with 'pip install', pip creates
+    #     an "isolated" build environment, without cupy installed (even if cupy
+    #     is already installed in the "main" environment).
+    #
+    #   - This triggers 'pip install cupy' in the build env (not the main env).
+    #
+    #   - Since pypi cupy is a source distributionb, not a precompiled distribution,
+    #     this takes forever and is unlikely to work.
+    #
+    # (Note: launch_busy_wait_kernel() is the only function in ksgpu which uses cupy.)
+    
+    import cupy
     
     ksgpu_pybind11._launch_busy_wait_kernel(arr, a40_seconds, cupy.cuda.get_current_stream().ptr)
