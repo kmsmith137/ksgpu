33a74e3fbc13abc9d6735ab343c37b9fe338fba6
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2025-11-20 12:21:01 +0000

    time-shared-memory: int2, int4

--- src_bin/time-shared-memory.cu
+++ src_bin/time-shared-memory.cu
@@ -8,39 +8,41 @@ using namespace std;
 using namespace ksgpu;
 
 
-// -------------------------------------------------------------------------------------------------
-//
+__device__ inline int _reduce(int x) { return x; }
+__device__ inline int _reduce(int2 x) { return x.x ^ x.y; }
+__device__ inline int _reduce(int4 x) { return x.x ^ x.y ^ x.z ^ x.w; }
+
+
 // 'p' should be a zeroed array of length (threads/block) * (blocks/kernel).
 // Kernels should be launched with shmem_nbytes = 4 * (threads/block).
-
-
-template<bool Read, bool Write>
-__global__ void shmem_read_write_kernel(int *p, int niter)
+template<typename T, bool Read, bool Write>
+__global__ void shmem_read_write_kernel(T *p, int niter)
 {
     constexpr int N = (Read && Write) ? 2 : 1;
-    extern __shared__ int shmem[];
+    extern __shared__ int shmem_[];
+    T *shmem = (T *) shmem_;
 
     p += threadIdx.x + (blockIdx.x * blockDim.x);
-    int x = *p;
+    T x = *p;
     
     int s = threadIdx.x;
     shmem[s] = x;
     
     for (int i = 2; i < niter; i += N) {
-	int y = Read ? shmem[s] : (x >> 1);
-	x ^= y;
+	T y = Read ? shmem[s] : x;
 	
-	if (Write)
+	if constexpr (Write)
 	    shmem[s] = x;
-	
-	s ^= x;
+
+	// Doesn't actually change the value of s, but the compiler doesn't know that.
+	s = s ^ (_reduce(y) & i);
     }
 
     *p = shmem[s];
 }
 
 
-template<bool Read, bool Write>
+template<typename T, bool Read, bool Write>
 static void time_kernel(const char *name)
 {
     const int niter = 8 * 1024;
@@ -51,16 +53,16 @@ static void time_kernel(const char *name)
 
     const double sm_cycles_per_second = get_sm_cycles_per_second();
     const double instructions_per_kernel = double(niter) * double(threads_per_block/32) * double(blocks_per_kernel);
-    const double shmem_tb_per_kernel = 1.0e-12 * 128.0 * instructions_per_kernel;
-    const int shmem_nbytes = 4 * threads_per_block;
+    const double shmem_tb_per_kernel = 1.0e-12 * 32.0 * sizeof(T) * instructions_per_kernel;
+    const int shmem_nbytes = threads_per_block * sizeof(T);
 
-    Array<int> arr({nstreams, blocks_per_kernel * threads_per_block }, af_gpu | af_zero);
+    Array<uint8_t> arr({nstreams, blocks_per_kernel * threads_per_block * sizeof(T) }, af_gpu | af_zero);
     
     auto callback = [&](const CudaStreamPool &pool, cudaStream_t stream, int istream)
     {
-	int *gmem = arr.data + (istream * arr.shape[1]);
+	T *gmem = (T*)arr.data + (istream * arr.shape[1]);
 	
-	shmem_read_write_kernel<Read, Write>
+	shmem_read_write_kernel<T, Read, Write>
 	    <<< blocks_per_kernel, threads_per_block, shmem_nbytes, stream >>>
 	    (gmem, niter);
 	
@@ -76,9 +78,17 @@ static void time_kernel(const char *name)
     
 int main(int argc, char **argv)
 {
-    time_kernel<true,false> ("Read shared memory");
-    time_kernel<false,true> ("Write shared memory");
-    time_kernel<true,true> ("Read/write shared memory");
+    time_kernel<int,true,false> ("Read shared memory (int)");
+    time_kernel<int,false,true> ("Write shared memory (int)");
+    time_kernel<int,true,true> ("Read/write shared memory (int)");
+    
+    time_kernel<int2,true,false> ("Read shared memory (long)");
+    time_kernel<int2,false,true> ("Write shared memory (long)");
+    time_kernel<int2,true,true> ("Read/write shared memory (long)");
+    
+    time_kernel<int4,true,false> ("Read shared memory (long)");
+    time_kernel<int4,false,true> ("Write shared memory (long)");
+    time_kernel<int4,true,true> ("Read/write shared memory (long)");
     
     return 0;
 }
